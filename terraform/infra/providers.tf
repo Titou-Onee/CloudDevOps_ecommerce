b8232e47d82ea967bcc0b2f5c6b879219a51bc80
Titou-onee <tmauchamp.0@gmail.com>
2025-10-15 23:53:52 +0200

    Fix Argocd apps repository

--- terraform/infra/providers.tf
+++ terraform/infra/providers.tf
@@ -1,13 +1,13 @@
 terraform {
   required_version = ">= 1.11"
 
-  backend "s3" {
-  bucket         = var.bucket_name
-  key            = "projet/terraform.tfstate"
-  region         = "eu-west-3"
-  use_lockfile = true
-  profile = "default"
-  }
+  #backend "s3" {
+  #bucket         = var.bucket_name
+  #key            = "projet/terraform.tfstate"
+  #region         = "eu-west-3"
+  #use_lockfile = true
+  #profile = "default"
+  #}
 }
 
 provider "aws" {
