9b5e03def1bce45cade9f5e44b7b43d4e39b542a
Titou-onee <tmauchamp.0@gmail.com>
2025-11-13 18:09:04 +0100

    fix app

--- Docker/ecommerce_store/settings.py
+++ Docker/ecommerce_store/settings.py
@@ -98,8 +98,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
-        "USER": "admin-user",
-        "PASSWORD": "unsecure-admin-password",
+        "USER": "postgres",
+        "PASSWORD": "django-pass",
         "HOST": "postgres-service",
         "PORT": 5432,
     }
