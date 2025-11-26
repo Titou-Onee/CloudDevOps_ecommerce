4432d76e7a85798a5ceb34886a9f02b49348e36a
Titou-Onee <tmauchamp.0@gmail.com>
2025-11-26 11:29:19 +0100

    test

--- terraform/infra/modules/eks/main.tf
+++ terraform/infra/modules/eks/main.tf
@@ -1,5 +1,5 @@
 
-# Eks cluster defination and IAM
+# Eks cluster defination and IAM 
 resource "aws_eks_cluster" "main" {
     name = var.cluster_name
     role_arn = aws_iam_role.eks_cluster_role.arn
